08d6b2d40478ca7da609290a5aada2707749f1f3
STEVE TENAS <stevetenadjang@gmail.com>
2021-04-16 06:21:44 +0100

    - Create owner Controller

---
+++ RealEstateApp.API/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Http;
+
+namespace RealEstateApp.API.Controllers
+{
+    public class BaseController : ApiController
+    {
+        protected readonly Entities.Entities db;
+
+        public BaseController()
+        {
+            db = new Entities.Entities();
+        }
+    }
+}
---
+++ RealEstateApp.API/Controllers/OwnerController.cs
@@ -0,0 +1,63 @@
+﻿using RealEstateApp.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RealEstateApp.API.Controllers
+{
+    public class OwnerController : BaseController
+    {
+        [HttpGet]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok( await db.Owners.ToArrayAsync());
+        }
+
+        [HttpGet]
+        public async Task<IHttpActionResult> Details(int id)
+        {
+            return Ok( await db.Owners.FindAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Post([FromBody] Owner item)
+        {
+            item.CreatedDate = DateTime.Now;
+            db.Owners.Add(item);
+            await db.SaveChangesAsync();
+
+            return Ok(item);
+        }
+
+        [HttpPut]
+        public async Task<IHttpActionResult> Put([FromBody] Owner item)
+        {
+            var oldItem = await db.Owners.AsNoTracking().FirstOrDefaultAsync(x => x.Id == item.Id);
+            if(oldItem != null)
+            {
+                item.CreatedDate = oldItem.CreatedDate;
+                db.Entry(item).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+            }
+            return Ok(item);
+        }
+
+
+        [HttpDelete]
+        public async Task<IHttpActionResult> Delete(int id)
+        {
+            var item = await db.Owners.FindAsync(id);
+            if (item != null)
+            {
+                db.Owners.Remove(item);
+                await db.SaveChangesAsync();
+            }
+            return Ok(item);
+        }
+    }
+}
